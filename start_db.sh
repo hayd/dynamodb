5eaa8300bdb55b59573f3e67902198838f16beaa
Andy Hayden <andyhayden1@gmail.com>
2019-12-09 21:43:11 -0800

    x

--- start_db.sh
+++ start_db.sh
@@ -8,7 +8,7 @@ DIR="./dynamodb_local_latest"
 if [[ ! -d $DIR ]]; then
   deno eval 'Deno.mkdir("$DIR")';
   curl --progress-bar $URL -o 'dynamodb_local_latest.tar.gz';
-  tar zxvf 'dynamodb_local_latest.tar.gz' --directory="$DIR"
+  tar -z -x -v -f 'dynamodb_local_latest.tar.gz' --directory="$DIR"
 fi
 
 java -D"java.library.path=$DIR/DynamoDBLocal_lib" -jar "$DIR/DynamoDBLocal.jar" -sharedDb &
